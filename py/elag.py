040f8b237994902a7ac37e86143917e096ea7045
Balogh Tivadar <Baloght@KKSZKI.LOCAL>
2022-11-17 12:02:40 +0100

    3.fel kesz

--- py/elag.py
+++ py/elag.py
@@ -1,3 +1,4 @@
+import random
 '''State=input("Jó mapod van?")
 if State=="igen":
     print("ja")
@@ -5,4 +6,32 @@ elif State=="nem":
     print("oke")
 else:
     print("ilyen valasz nincs!")
+    
+    #3.fel
+    
+Number=input("kérek egy számot")
+if(Number%2==0):
+    print("szám páros")
+else:
+    print("páratlan")
     '''
+
+    #4.fel
+
+    from tokenize import Number
+
+
+    Number = random.randint(1,6)
+
+    userNumber=int(input("Kérek egy számot:"))
+    print(Number)
+
+    if(userNumber<Number):
+        print("Kisebb")
+    elif(userNumber>Number):
+    
+        print("nagyobb")
+    
+    else:
+    
+        print("Egyenlo")
